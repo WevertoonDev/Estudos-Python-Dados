e39b5d4b81b6dafd2b61c24116c20d3fb15104d4
WevertoonDev <wevertonm22@icloud.com>
2026-09-04 11:34:59 -0300

    Adiciona estudos iniciais de Python

---
+++ 01_fundamentos/aula_01.py
@@ -0,0 +1,14 @@
+# aula 01 Fundamentos exercicio 1
+print("Meu nome é Weverton," \
+"Estou estudando Python, " \
+"Quero trabalhar com programação")
+# exercicio 02
+nome = "Weverton"
+idade = 29
+profissao = "programador"
+print(nome, idade, profissao)
+# exercicio 03
+nome = "Weverton"
+idade = 29
+profissao = "programador"
+print("Meu nome é",nome, "e tenho", idade, "anos e quero ser", profissao)
---
+++ 04_excecoes/exercicio_01.py
@@ -0,0 +1,12 @@
+produtos = [
+    {"nome": "Notebook", "preco": 3500},
+    {"nome": "Mouse", "preco": 80},
+    {"nome": "Teclado", "preco": 150},
+    {"nome": "Monitor", "preco": 1200}
+]
+def produtos_promocao(produtos):
+    nova_lista = []
+    for produto in produtos:
+        if produto["preco"] > 100:
+            nova_lista.append(produto)
+    print (nova_lista)
