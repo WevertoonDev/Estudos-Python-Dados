969e1ba6684727a21cc056e47a3c02494c02d18b
WevertoonDev <wevertonm22@icloud.com>
2026-09-04 12:22:05 -0300

    aula 3 — Operações matemáticas exercício 07

--- 01_fundamentos/aula_01.py
+++ 01_fundamentos/aula_01.py
@@ -12,3 +12,32 @@ nome = "Weverton"
 idade = 29
 profissao = "programador"
 print("Meu nome é",nome, "e tenho", idade, "anos e quero ser", profissao)
+#aula 02 exercicio 04 
+nome = "Weverton"
+idade = 29
+altura = 1.80
+estudando = True
+print(type(nome), 
+      type(idade), 
+      type(altura), 
+      type(estudando))
+#aula 3 — Operações matemáticas exercício 05
+# soma = + exemplo 10 + 2 = 12
+# subtração = - exemplo 10 - 2 = 8
+# multiplicação = * exemplo 10 * 2 = 20
+# divisão / exemplo 10 / 2 = 5.0
+preco = 10 
+quantidade = 5
+resultado = preco * quantidade
+print("O resultado é ,", resultado)
+# exercício 06 
+valor = 100
+pessoas = 4
+resultado = valor / pessoas
+print("O resultado por pessoa é ,", resultado)
+# exercício 07
+numero = 17
+divisor = 5
+restultado = numero % divisor
+resto = resultado 
+print("O resto da divisao é ,", resto)
