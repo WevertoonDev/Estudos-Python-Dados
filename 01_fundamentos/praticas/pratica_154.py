61fa4ddb9ff6a642d20d917b6c5c8d397898b761
WevertoonDev <wevertonm22@icloud.com>
2026-09-24 14:39:49 -0300

    Pratica_154

---
+++ 01_fundamentos/praticas/pratica_154.py
@@ -0,0 +1,16 @@
+#pratica 154
+def classificar_compra(valor):
+    if valor >= 500:
+        return("Compra grande")
+    else:
+        return("Compra normal")
+resultado = classificar_compra(valor = 350)
+print(resultado)
+
+def classificar_idade(idade):
+    if idade >= 18:
+        return("Adulto")
+    else:
+        return("Menor de idade")
+resultado = classificar_idade(idade = 16)
+print(resultado)
